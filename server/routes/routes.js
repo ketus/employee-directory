a40d93c2c5abf334a7c8b647eb39ad0b23fc05ee
Ketus-home <maciej.oleksa@gmail.com>
2016-06-13 17:26:52 +0200

    .

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,6 +5,6 @@ module.exports = function(app, config) {
     app.use('/api', require('../routes/employees'));
 
     app.get('*', function(req, res) {
-        res.sendFile(path.join(config.rootPath, 'server', 'views', 'index.html'));
+        res.sendFile(path.join(config.rootPath, 'public', 'app', 'index.html'));
     });
 };
