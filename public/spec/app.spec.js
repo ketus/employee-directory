5acf2b4ccd5b9098eee4c9c43ca024b44be41b35
Ketus-home <maciej.oleksa@gmail.com>
2016-06-14 11:50:24 +0200

    add client-side karma and jasmine

---
+++ public/spec/app.spec.js
@@ -0,0 +1,22 @@
+describe('calculator', function () {
+
+  beforeEach(module('app'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));
+
+  describe('sum', function () {
+		it('1 + 1 should equal 2', function () {
+			var $scope = {};
+			var controller = $controller('EmployeeListController', { $scope: $scope });
+			$scope.x = 1;
+			$scope.y = 2;
+			$scope.z = 3;
+			expect($scope.z).toEqual(3);
+		});
+	});
+
+});
